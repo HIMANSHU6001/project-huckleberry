9dd0ccf1b3eab184ddfb5a5d317691ced86a94de
ayush <ayushatschool5@gmail.com>
2025-03-03 17:30:04 +0530

    refactor: remove Supabase authentication functions and related components

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 import React from 'react';
-import { loginWithGoogle } from '@/actions/auth';
 import { Button } from '@/components/ui/button';
 import { FcGoogle } from 'react-icons/fc';
+import { signIn } from '@/auth';
 
 export default function LoginPage() {
   return (
     <section className="flex items-center justify-center min-h-screen ">
-      <Button variant="outline" onClick={loginWithGoogle}>
+      <Button variant="outline" onClick={() => signIn('google')}>
         <FcGoogle className="w-5 h-5" />
         <span>Continue with Google</span>
       </Button>
